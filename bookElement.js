447e45dccbceae0f9923844cb14b33293a5a3b57
savvanna dhanaraj sai prasanth <saiprasanth0012@gmail.com>
2023-12-23 22:35:59 +0530

    add delet functionlity

--- bookElement.js
+++ bookElement.js
@@ -1,3 +1,4 @@
+
 export default  function bookEle(book) {
   const liel = document.createElement('li')
   liel.innerHTML=`<h3> Title :${book.title}</h3> 
@@ -6,5 +7,12 @@ export default  function bookEle(book) {
                     <h3> Have you Read :${book.readStatus}</h3>
                     <button >Delet </button>`
   document.querySelector('ul').appendChild(liel);
-  delFun();
+  const delele = document.querySelectorAll('li button');
+  const del = Array.from(delele);
+  const lilist = Array.from(document.querySelectorAll('li'));
+  del.forEach((bun)=>{
+    bun.addEventListener('click',()=>{
+       lilist[del.indexOf(bun)].remove();
+    })
+  })
 }
